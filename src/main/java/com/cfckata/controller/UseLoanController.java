251ae8f5a228e6aeb74290d6fbcfc62cf2565c64
supk <12345678>
2020-09-11 11:33:59 +0800

    用信申请业务类

---
+++ src/main/java/com/cfckata/controller/UseLoanController.java
@@ -0,0 +1,10 @@
+package com.cfckata.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/useLoan")
+public class UseLoanController {
+
+}
---
+++ src/main/java/com/cfckata/service/UseLoanService.java
@@ -0,0 +1,9 @@
+package com.cfckata.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UseLoanService {
+
+}
