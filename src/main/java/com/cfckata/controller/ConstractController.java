0a465df81b9a4ff0f349b5241c57d4bd967d1dbb
lihaibozb <lihaibo19861126@126.com>
2020-09-11 11:17:02 +0800

    constract

--- src/main/java/com/cfckata/controller/ConstractController.java
+++ src/main/java/com/cfckata/controller/ConstractController.java
@@ -1,7 +1,7 @@
 package com.cfckata.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/constract")
